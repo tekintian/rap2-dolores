be6c149c7328fc872d7e572ba0c005c61a6a6cab
tekintian <tekintian@gmail.com>
2018-12-29 09:44:59 +0800

    增加默认 key 配置

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -1,6 +1,8 @@
 module.exports = {
-  serve: 'http://rap.soullv.com:8003',
-  keys: ['some secret hurr'],
+	//  rap2为 rap2-delos的部署服务器容器连接名称，或者是你的rap2服务器的的地址与端口 
+  serve: 'http://192.168.99.100:8080',
+  // 设置你的服务端的安全连接key, 此key 的定义在你的RAP2服务端 src/config/config.*.js 文件中定义
+  keys: ['sJLIgP0iJchKLz7pxlYHTOxmZRwdTjn4kVst7q7pOxEN41eqA4sV5pM3'],
   session: {
     key: 'koa:sess'
   }
--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,7 +1,9 @@
 module.exports = {
-//  rap2为 rap2-delos的部署服务器容器连接名称，或者是你的容器的地址
-  serve: 'http://rap.soullv.com:8003',
-  keys: ['n4kVst7q7pOxEN41eqA4sV5pMJchKLz7pxlYHTOxmZRwdTj3sJLIgP0i'],
+//  rap2为 rap2-delos的部署服务器容器连接名称，或者是你的rap2服务器的的地址与端口 
+//  【如果是正式发布，则此地址必须位于公网能访问的地址，否则此客户端无法正常运行】
+  serve: 'http://192.168.99.100:8080',
+  // 设置你的服务端的安全连接key, 此key 的定义在你的RAP2服务端 src/config/config.*.js 文件中定义
+  keys: ['sJLIgP0iJchKLz7pxlYHTOxmZRwdTjn4kVst7q7pOxEN41eqA4sV5pM3'],
   session: {
     key: 'koa:sess'
   }
